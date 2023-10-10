8e498082d12292210ceb8cea069a82317fa46a5f
Jeki Maulana <jekibus@gmail.com>
2023-10-10 15:54:55 +0700

    Hotfix: change ip

--- v0.0.2/node1/run.sh
+++ v0.0.2/node1/run.sh
@@ -4,8 +4,8 @@ geth --datadir data \
     --networkid 1337 --nodiscover --verbosity 5 \
     --syncmode full --nousb \
     --raft --raftport 53001 --raftblocktime 300 --emitcheckpoints \
-    --http --http.addr 172.31.34.98 --http.port 22001 --http.corsdomain "*" --http.vhosts "*" \
-    --ws --ws.addr 172.31.34.98 --ws.port 32001 --ws.origins "*" \
+    --http --http.addr 172.31.44.251 --http.port 22001 --http.corsdomain "*" --http.vhosts "*" \
+    --ws --ws.addr 172.31.44.251 --ws.port 32001 --ws.origins "*" \
     --http.api admin,trace,db,eth,debug,miner,net,shh,txpool,personal,web3,quorum,raft \
     --ws.api admin,trace,db,eth,debug,miner,net,shh,txpool,personal,web3,quorum,raft \
     --unlock ${ADDRESS} --allow-insecure-unlock --password ./data/keystore/accountPassword \
