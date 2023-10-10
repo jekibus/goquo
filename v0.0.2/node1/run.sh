ea4da32cae54b432cb97b1bc6d8866fefb235aea
Jeki Maulana <jekibus@gmail.com>
2023-10-10 15:23:48 +0700

    Change IP to support difference vm

---
+++ v0.0.2/node1/run.sh
@@ -0,0 +1,12 @@
+export ADDRESS=$(grep -o '"address": *"[^"]*"' ./data/keystore/accountKeystore | grep -o '"[^"]*"$' | sed 's/"//g')
+export PRIVATE_CONFIG=ignore
+geth --datadir data \
+    --networkid 1337 --nodiscover --verbosity 5 \
+    --syncmode full --nousb \
+    --raft --raftport 53001 --raftblocktime 300 --emitcheckpoints \
+    --http --http.addr 172.31.34.98 --http.port 22001 --http.corsdomain "*" --http.vhosts "*" \
+    --ws --ws.addr 172.31.34.98 --ws.port 32001 --ws.origins "*" \
+    --http.api admin,trace,db,eth,debug,miner,net,shh,txpool,personal,web3,quorum,raft \
+    --ws.api admin,trace,db,eth,debug,miner,net,shh,txpool,personal,web3,quorum,raft \
+    --unlock ${ADDRESS} --allow-insecure-unlock --password ./data/keystore/accountPassword \
+    --port 30301
