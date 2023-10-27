0d1d895fdaa0a5f6473d3986b83d5b7725fbc93a
Jeki Maulana <jekibus@gmail.com>
2023-10-27 09:30:16 +0700

    Change raft id

--- v0.0.3/node2/run.sh
+++ v0.0.3/node2/run.sh
@@ -3,7 +3,7 @@ export PRIVATE_CONFIG=ignore
 geth --datadir data \
     --networkid 1337 --nodiscover --verbosity 5 \
     --syncmode full --nousb \
-    --raft --raftport 53002 --raftblocktime 300 --raftjoinexisting 2 --emitcheckpoints \
+    --raft --raftport 53002 --raftblocktime 300 --raftjoinexisting 3 --emitcheckpoints \
     --http --http.addr 172.31.16.26 --http.port 22002 --http.corsdomain "*" --http.vhosts "*" \
     --ws --ws.addr 172.31.16.26 --ws.port 32002 --ws.origins "*" \
     --http.api admin,trace,db,eth,debug,miner,net,shh,txpool,personal,web3,quorum,raft \
